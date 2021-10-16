eec1719ec5642e5aefa9686924daf93f5451fe3b
efendy <efendy.setiawan@gmail.com>
2021-10-16 22:56:28 +0800

    settings ready

--- src/js/view_home.js
+++ src/js/view_home.js
@@ -1,5 +1,17 @@
 console.log('view_home.js is READY');
 
+$('#datepicker-home-from').datepicker({
+  uiLibrary: 'bootstrap'
+});
+$('#datepicker-home-to').datepicker({
+  uiLibrary: 'bootstrap4'
+});
+
+applySelectAllEvents(document, "source");
+applySelectAllEvents(document, "destination");
+applySelectItemEvents(document, "source");
+applySelectItemEvents(document, "destination");
+
 
 var home = {
   search: function() {
@@ -24,5 +36,31 @@ var home = {
   },
   delete: function() {
     
+  },
+  init: function() {
+    // create table on source
+    // create table on destination
+    // CREATE TABLE IF NOT EXISTS tbl_transaksi;
+  }
+}
+
+function applySelectAllEvents(doc, target) {
+  // console.log("applySelectAllEvents:",target);
+  doc.getElementsByClassName("select-"+target+"-all")[0].onclick = function() { 
+    // console.log("select-"+target+"-all checked?",this.checked);
+    let items = doc.getElementsByClassName("select-"+target+"-item");
+    for (var i=0; i < items.length; i++) {
+      items[i].checked = this.checked;
+    }
+  };
+}
+
+function applySelectItemEvents(doc, target) {
+  console.log("applySelectItemEvents:",target);
+  let selectItems = doc.getElementsByClassName("select-"+target+"-item");
+  for (var i=0; i < selectItems.length; i++) {
+    selectItems[i].onclick = function() {
+      doc.getElementsByClassName("select-"+target+"-all")[0].checked = false;
+    }
   }
 }
